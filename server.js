1d37d9daca5f35f3bffc47c95e037d70c9cb230a
iqbal kang <iqbal-kang@iqbals-MacBook-Air.local>
2023-06-01 19:39:20 -0700

    fixed dotenv file error

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const app = require('./app')
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-dotenv.config({ path: './config.env' })
-
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
-  await mongoose.connect(process.env.MONGO_URL)
-  app.listen(PORT, () => console.log('Listening on ' + PORT + '...'))
-}
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(PORT, () => console.log('Listening on ' + PORT + '...'));
+};
 
-startServer()
+startServer();
